1d78bcfbb315ee46e69f3e0f5612326a01234236
Jack <jackryan1998@hotmail.com>
2019-09-13 20:43:25 +1000

    Update LinkedList.cpp

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -20,16 +20,50 @@ LinkedList::~LinkedList()
 
 int LinkedList::length()
 {
-
+    int length = 0;
+    Node* temp = head;
+    while (temp->next != nullptr)
+    {
+        length++;
+        temp = temp->next;
+    }
+    return length;
+    
 }
 
 void LinkedList::addLast(Tile* tile)
 {
+    Node* newNode = new Node(tile, nullptr);
+
+    //check if head exists
+    if (head == nullptr)
+    {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+
+    //check if head is only node
+    if(length() == 1){
+        head->next = newNode;
+    }
+    while (temp->next != nullptr)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+
 
 }
 
 void LinkedList::addFirst(Tile * tile) {
-
+    Node *newNode = new Node(tile, nullptr);
+    if(head == nullptr) {
+        head = newNode;
+    }
+    else {
+        newNode->next = head;
+    }
 }
 
 Tile* LinkedList::removeFirst()
