a499cbd9987424225536a805e871a510481a83db
Jack <jackryan1998@hotmail.com>
2019-10-06 21:39:42 +1100

    Revert "Update Board.cpp"
    This reverts commit cfae0c160ee3d8ec836770e04d64e5c718a50737.

--- Board.cpp
+++ Board.cpp
@@ -179,6 +179,7 @@ bool Board::makeMove(Player& player, int row, int col, Tile* tile) {
             this->setTile(row,col,tile->getColour(),tile->getShape());
             success = true;
         }
+    }
         int score = getMovePoints(row,col);
         player.setScore(score);
         if (score == 6) {
@@ -186,8 +187,6 @@ bool Board::makeMove(Player& player, int row, int col, Tile* tile) {
             player.setScore(score);
         }
         return success;
-    }
-    return success;  
     }
 
 int Board::getMovePoints(int placedRow, int placedCol)
